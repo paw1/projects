987b72b94b0b26c5aa3085ca478f27c5f3adb0e0
pawan <pa1.annoy.nymous@gmail.com>
2018-04-21 01:06:46 +0530

    added gifs

--- temp/js/custom.js
+++ temp/js/custom.js
@@ -34,3 +34,25 @@ function pauseGif(num) {
     z.setAttribute('src', z.getAttribute('data-srcjpg'))
 
 }
+
+function playmp4(num) {
+
+    let x = document.getElementById(`gif${num}playbutton`)
+    x.style.display = "none"
+    let y = document.getElementById(`gif${num}pausebutton`)
+    y.style.display = "inline"
+
+    let z = document.getElementById(`video${num}`)
+    z.play()
+}
+
+function pausemp4(num) {
+
+    let x = document.getElementById(`gif${num}playbutton`)
+    x.style.display = "inline"
+    let y = document.getElementById(`gif${num}pausebutton`)
+    y.style.display = "none"
+
+    let z = document.getElementById(`video${num}`)
+    z.pause()
+}
